89b968609e3233c8e61ca2f89e2e549fe35ea861
Yogi Dinata <yogirpl22@gmail.com>
2025-07-16 09:10:41 +0000

    tidak bisa ada yang eror

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // For more information, see: https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
-  apiKey: "AIzaSyAwwTrt3dPC7oICozC6pGvb-xFYm8c3dHg",
+  apiKey: "AIzaSyAEGE0OwI8spapTkjF1_r3k9ygKhByAiNs",
   authDomain: "intorma.firebaseapp.com",
   projectId: "intorma",
   storageBucket: "intorma.appspot.com",
   messagingSenderId: "1022928523995",
-  appId: "1:1022928523995:web:b66aecf90e7599cf01b2fe"
+  appId: "1:1022928523995:web:a2883101a40c0a04cbf0a2"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
