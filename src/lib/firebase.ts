1c821c8c52703f110a52a891c099ac75112e7589
Yogi Dinata <yogirpl22@gmail.com>
2025-07-16 08:55:31 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAEGE0OwI8spapTkjF1_r3k9ygKhByA

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // For more information, see: https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAEGE0OwI8spapTkjF1_r3k9ygKhByAiNs",
+  authDomain: "kerjasini.firebaseapp.com",
+  projectId: "kerjasini",
+  storageBucket: "kerjasini.appspot.com",
+  messagingSenderId: "60196811271",
+  appId: "1:60196811271:web:a2883101a40c0a04cbf0a2"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
