fe22a96b12aba34b6a5f84f0d6bf7c5c71c924db
Yogi Dinata <yogirpl22@gmail.com>
2025-08-13 02:14:08 +0000

    masihhh ini

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -17,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-4 sm:bottom-4 sm:right-4 sm:top-auto left-1/2 -translate-x-1/2 sm:left-auto sm:right-auto sm:translate-x-0 z-[200] flex max-h-screen w-full flex-col-reverse p-4 md:max-w-md",
+      "fixed top-4 left-1/2 -translate-x-1/2 sm:bottom-4 sm:right-4 sm:top-auto sm:left-auto sm:translate-x-0 z-[200] flex max-h-screen w-full flex-col-reverse p-4 md:max-w-md",
       className
     )}
     {...props}
